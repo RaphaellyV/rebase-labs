7a5b0b8d194501fc0f3d2b905b85c585d548342e
Raphaelly V <valiengo.raphaelly@yahoo.com>
2023-01-03 14:14:29 -0300

    Ajustando teste para visualização da tabela de testes

--- spec/user_views_tests_table_spec.rb
+++ spec/user_views_tests_table_spec.rb
@@ -17,36 +17,6 @@ describe 'Usuário vê tabela de testes' do
     expect(page).to have_content 'Data'
     expect(page).to have_content 'Limites'
     expect(page).to have_content 'Resultado'
-  end
-
-  it 'e vê lista de exames' do
-    conn = PG.connect(host: 'postgres', dbname: 'postgres', user: 'postgres')
-    QueryService.new.import_csv('./spec/support/tests.csv')
-
-    visit '/tests-table'
-
-    expect(page).to have_content 'IQCZ17'
-    expect(page).to have_content 'Emilly Batista Neto'
-    expect(page).to have_content '048.973.170-88'    
-    expect(page).to have_content '2001-03-11'
-    expect(page).to have_content 'B000BJ20J4/PI'
-    expect(page).to have_content 'Luiza Pires'
-    expect(page).to have_content 'hemácias'
-    expect(page).to have_content '2021-08-05'   
-    expect(page).to have_content '45-52'
-    expect(page).to have_content '97'
-    expect(page).to have_content 'leucócitos'
-    expect(page).to have_content '9-61'
-    expect(page).to have_content '89'
-
-    expect(page).to have_content '0W9I67'
-    expect(page).to have_content 'Juliana dos Reis Filho'
-    expect(page).to have_content '048.108.026-04'    
-    expect(page).to have_content '1995-07-03'
-    expect(page).to have_content 'B0002IQM66/SC'
-    expect(page).to have_content 'Maria Helena Ramalho'
-    expect(page).to have_content '2021-07-09'
-    expect(page).to have_content '28'
-    expect(page).to have_content '91'    
+    expect(page.body).to include('script src="tests_table.js"')
   end
 end
